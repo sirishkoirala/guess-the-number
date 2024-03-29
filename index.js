2f64e02a691424b7e6ad226b76a67d685394dd7f
Sirish Koirala <69025172+sirishkoirala@users.noreply.github.com>
2024-03-29 19:44:34 +0545

    Create index.js
    check whether the entered number is equal to the random number

---
+++ index.js
@@ -0,0 +1,13 @@
+let random = Math.floor(Math.random() * 100);
+let count = 1;
+let guess = prompt('Enter a number between 1 to 100');
+while (guess != random) {
+    console.log("the number is",random < guess   ? "less than" + guess : "grater than " + guess)
+    guess = prompt('guess the number agian:')
+    count = count + 1
+}
+console.log('number matched congratulations');
+console.log('you took ' + count + " count");
+
+//run this in your browwsers console
+
